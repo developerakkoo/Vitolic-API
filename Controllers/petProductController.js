66dbad171db17b01ba37544835a08472d095b19c
Akshay <developerakshayjadhav@gmail.com>
2022-07-21 01:01:31 +0530

    Akshay Added pet product modal controler

---
+++ Controllers/petProductController.js
@@ -0,0 +1,64 @@
+const Pet = require('./../Models/petProductModal');
+
+
+exports.PetProductFilter = async(req, res,next) => {
+    try {
+       
+
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+
+exports.postPetProduct = async(req, res,next) => {
+    try {
+        //create a new pet product
+
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+exports.getPetProduct = async(req, res,next) => {
+    try {
+       
+
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+
+exports.getPetProductById = async(req, res,next) => {
+    try {
+       
+
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+exports.updatePetProduct = async(req, res,next) => {
+    try {
+        
+
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+exports.deletePetProduct = async(req, res,next) => {
+    try {
+       
+
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
---
+++ Models/petProductModal.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+const petProductSchema = new Schema({
+
+    title:{
+        type: String,
+        required: [true, 'title is required'],
+    },
+
+    price:{
+        type: Number,
+        required: [true, 'price is required'],
+
+    },
+
+    discountedPrice:{
+        type: Number,
+        required: [true, 'discountedPrice is required'],
+
+    },
+
+    quantity:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Quantity'
+        }
+    ],
+    
+    inStock:{
+        type: String,
+        required: [true, 'inStock is required']
+    },
+    category:{
+        type: String,
+        required: [true, 'category is required'],
+       
+    },
+
+    subCategory:{
+        type: String,
+        required: [true, 'sub category is required'],
+       
+    },
+
+    stock:{
+        type: Number,
+        required: [true, 'stock is required']
+    },
+
+    units:{
+        type: String,
+        default: 'item'
+    },
+
+    imageUrl:{
+        type: String,
+        default: 'https://image.shutterstock.com/image-illustration/default-white-background-template-without-600w-1971913538.jpg'
+    },
+
+    imageFilePath:{
+        type: String,
+    }
+
+},
+{timestamps: true}
+);
+
+
+module.exports = mongoose.model('PetProduct', petProductSchema);
