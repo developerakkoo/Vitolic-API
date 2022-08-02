b289e66e4988964922c091d436b307fdf728d9cf
ekko14 <raj@plenfore.com>
2022-08-02 10:54:53 +0530

    order doc removed

--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -5,47 +5,6 @@ const apicache = require('apicache');
 const cache = apicache.middleware;
 
 
-/**
- * @swagger
- * /order:
- *  post:
- *      description: Add order
- *      tags:
- *          - order
- * 
- *      parameters:
- *          - name: products
- *  
- *          - name: product
- *            description: products to order
- *            in: formData
- *            required: true
- *            type: String 
- *          - name: quantity
- *            description: quantity to order
- *            in: formData
- *            required: true
- *            type: Number ]    
- *       
- *           
- *          - name: products
- *            description: products to order
- *            in: formData
- *            required: true
- *            type: String
- *          - name: products
- *            description: products to order
- *            in: formData
- *            required: true
- *            type: String
- *
- *      responses:
- *         200:
- *              description: Success
- *  
- *  
- * 
- */
 router.post('/order', cache('7 days'), orderController.createOrder);
 
 
