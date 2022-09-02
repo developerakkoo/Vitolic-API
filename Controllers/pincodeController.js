05f2137c1ef207c6656a43975e22b2f64c7ad7e3
ekko14 <raj@plenfore.com>
2022-09-02 15:51:32 +0530

    pincode fixed

--- Controllers/pincodeController.js
+++ Controllers/pincodeController.js
@@ -1,53 +1,63 @@
-const pincode = require('./../Models/pincodeModal');
+const Pincode = require('./../Models/pincodeModal');
 
-exports.createPincode = async(req, res, next) =>{
+exports.createPincode = async (req, res, next) => {
     try {
-        let pincode = await pincode.create(req.body);
+        const areaName = req.body.areaName;
+        const pincode = req.body.pincode;
 
-        if(pincode){
-            res.status(200).json({success: true, pincode})
-        }
+        const pincodes = new Pincode({
+            areaName,
+            pincode,
+        })
+
+        pincodes.save().then((result) => {
+            res.status(200).send(result);
+        })
     } catch (error) {
-        res.status(500).json({message: error.message, devMessage:"Something went wrong!"});
+        res.status(500).json({ message: error.message, devMessage: "Something went wrong!" });
     }
 }
 
-exports.getPincodes = async(req, res, next) =>{
+exports.getPincodes = async (req, res, next) => {
     try {
-        let pincode = await pincode.find({});
+        let pincode = await Pincode.find({});
 
-        if(pincode){
-            res.status(200).json({success: true, pincode})
+        if (pincode) {
+            res.status(200).json({ success: true, count:pincode.length, pincode })
         }
     } catch (error) {
-        res.status(500).json({message: error.message, devMessage:"Something went wrong!"});
-    }
-}
-
-
-exports.updatePincode = async(req, res, next) =>{
-    try {
-        let pincode = await pincode.findOneAndUpdate(req.params.id,
-            req.body);
-
-        if(pincode){
-            res.status(200).json({success: true, pincode})
-        }
-    } catch (error) {
-        res.status(500).json({message: error.message, devMessage:"Something went wrong!"});
+        res.status(500).json({ message: error.message, devMessage: "Something went wrong!" });
     }
 }
 
 
-exports.deletePincode = async(req, res, next) =>{
-    try {
-        let pincode = await pincode.findByIdAndDelete(req.params.id);
 
-        if(pincode){
-            res.status(200).json({success: true, pincode})
+
+exports.updatePincode = async (req, res, next) => {
+    try {
+        const id=req.params.id;
+        //const pincode = req.body.pincode;
+
+        let pincodes = await Pincode.findOneAndUpdate(id);
+
+        if (pincodes) {
+            res.status(200).json({ success: true, pincodes })
         }
-        
     } catch (error) {
-        res.status(500).json({message: error.message, devMessage:"Something went wrong!"});
+        res.status(500).json({ message: error.message, devMessage: "Something went wrong!" });
+    }
+}
+
+
+exports.deletePincode = async (req, res, next) => {
+    try {
+        let pincode = await Pincode.findByIdAndDelete(req.params.id);
+
+        if (pincode) {
+            res.status(200).json({ success: true, pincode })
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: error.message, devMessage: "Something went wrong!" });
     }
 }
