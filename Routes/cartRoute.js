59ef48188e04a012c358115ed74140131575b119
ekko14 <raj@plenfore.com>
2022-09-21 17:35:53 +0530

    bill

--- Routes/cartRoute.js
+++ Routes/cartRoute.js
@@ -4,6 +4,47 @@ const cartController = require('../Controllers/cartController');
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /cart:
+ *  post:
+ *      description: Add to cart
+ *      tags:
+ *          - cart
+ * 
+ *      parameters:
+ *          - name: userId
+ *            description: Id of the user
+ *            in: formData
+ *            required: true
+ *            type: String
+ *          - name: address
+ *            description: Address of the user
+ *            in: formData
+ *            required: true
+ *            type: String
+ *          - name: status
+ *            description: status of the cart
+ *            in: formData
+ *            required: true
+ *            type: String
+ *          - name: total
+ *            description: total amount in the cart
+ *            in: formData
+ *            required: true
+ *            type: Number
+ *
+ *      responses:
+ *         200:
+ *              description: Success
+ *  
+ *  
+ * 
+ */
+ router.post('/cart',  cartController.addToCart);
+
+ 
 /**
  * @swagger
  * /cart/featured:
