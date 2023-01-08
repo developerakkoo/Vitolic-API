afb835680c115dad9e30cdd7d0aadc4d7a90d9e9
Akshay Jadhav <akkooakshay@Apples-MacBook-Air.local>
2023-01-08 18:18:00 +0530

    changes

--- Models/subOrderModel.js
+++ Models/subOrderModel.js
@@ -3,29 +3,9 @@ const Schema = mongoose.Schema;
 
 
 
-const itemSchema = new Schema({
-    products: [{
 
-    }],
-    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+const subOrderSchema = new Schema({
 
-
-
-    total: {
-        type: Number,
-        required: true
-    }
-}, {
-    timestamps: true
-})
-
-module.exports = mongoose.model("Item", itemSchema);
-
-
-const cartSchema = new Schema({
-    products: [{
-
-    }],
     userId: { type: Schema.Types.ObjectId, ref: 'User' },
     address: { type: Schema.Types.ObjectId, ref: 'Address' },
     pincode: { type: String },
@@ -45,7 +25,6 @@ const cartSchema = new Schema({
         default: false
     },
     orderDate: { type: String },
-    subOrders:[{type: Schema.Types.ObjectId, ref:"subOrder"}],
     orderDays: { type: Array, "default": [] },
     type: {
         type: String,
@@ -66,4 +45,4 @@ const cartSchema = new Schema({
 });
 
 
-module.exports = mongoose.model('Cart', cartSchema);
+module.exports = mongoose.model('subOrder', subOrderSchema);
