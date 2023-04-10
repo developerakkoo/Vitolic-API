f5170c3e78b3b38208627e51ce3a45903b576fb4
Akshay Jadhav <akkooakshay@Apples-MacBook-Air.local>
2023-04-10 13:47:29 +0530

    new

--- Controllers/pincodeController.js
+++ Controllers/pincodeController.js
@@ -4,10 +4,28 @@ exports.createPincode = async (req, res, next) => {
     try {
         const areaName = req.body.areaName;
         const pincode = req.body.pincode;
+        const deliveryState = req.body.deliveryState;
+        const divisionName = req.body.divisionName;
+        const region = req.body.region;
+        const taluka = req.body.taluka;
+        const district = req.body.district;
+        const state = req.body.state;
+        const center = req.body.center;
+        const route = req.body.route;
+        const deliveryPerson = req.body.deliveryPerson;
 
         const pincodes = new Pincode({
             areaName,
             pincode,
+            deliveryState,
+            divisionName,
+            region,
+            taluka,
+            district,
+            state,
+            center,
+            route,
+            deliveryPerson
         })
 
         pincodes.save().then((result) => {
@@ -49,7 +67,7 @@ exports.updatePincode = async (req, res, next) => {
         const id = req.params.id;
         //const pincode = req.body.pincode;
 
-        let pincodes = await Pincode.findOneAndUpdate(id);
+        let pincodes = await Pincode.findOneAndUpdate(id, req.body);
 
         if (pincodes) {
             res.status(200).json({ success: true, pincodes })
--- Models/pincodeModal.js
+++ Models/pincodeModal.js
@@ -6,6 +6,16 @@ const Schema = mongoose.Schema;
 const pincodeSchema = new Schema({
     areaName: { type: String, },
     pincode: { type: String, },
+    deliveryState: { type: String, },
+    divisionName: { type: String, },
+    region: { type: String, },
+    taluka: { type: String, },
+    district: { type: String, },
+    state: { type: String, },
+    center: { type: String, },
+    route: { type: String, },
+    deliveryPerson: { type: Schema.Types.ObjectId, ref:"DeliveryBoy" },
+
 }, { timestamps: true });
 
 
